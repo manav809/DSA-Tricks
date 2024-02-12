0c5249c7515a34e01d6df915b0b2c3b848ea40c7
manav809 <manavpatel6978@gmail.com>
2024-02-11 20:50:25 -0500

    Add Ex 4 Sorting Dictionary

---
+++ Lambda/ex4.py
@@ -0,0 +1,5 @@
+x = [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
+
+x.sort(key=lambda y: y['color'])
+
+print(x)
