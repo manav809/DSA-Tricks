25b68f87293deaa59b817f7bab363221cd69438f
manav809 <manavpatel6978@gmail.com>
2024-06-11 22:08:18 -0400

    Add Binary Search

---
+++ Grind169/binary_search.py
@@ -0,0 +1,17 @@
+def search(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    l = 0
+    r = len(nums) - 1
+    while l <= r:
+        mid = (l + r) / 2
+        if nums[mid] < target:
+            l = mid + 1
+        elif nums[mid] > target:
+            r = mid - 1
+        else:
+            return mid
+    return -1
