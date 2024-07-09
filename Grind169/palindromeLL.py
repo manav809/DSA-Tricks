38f76132b9d8482fa844ea807401d4d9e3834117
manav809 <manavpatel6978@gmail.com>
2024-07-08 23:58:47 -0400

    Fix Issue

--- Grind169/palindromeLL.py
+++ Grind169/palindromeLL.py
@@ -4,7 +4,7 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+    def isPalindrome(self, head):
         slow = head
         fast = head
 
